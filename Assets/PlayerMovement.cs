48c07fdca71cee6081620a74748bff67ab4a59dd
aleyhin1 <aleyhin1@gmail.com>
2023-10-08 03:05:08 +0300

    Move method created and binded

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,11 +7,15 @@ public class PlayerMovement : NetworkBehaviour
 {
     public Camera _camera;
 
+    [SerializeField] private float _movementSpeed = 20;
+
+
     public override void FixedUpdateNetwork()
     {
         if (GetInput<NetworkInputData>(out var input))
         {
             Rotate(input.JoystickBackgroundPosition, input.JoystickHandlePosition);
+            Move(input.JoystickBackgroundPosition, input.JoystickHandlePosition);
         }
     }
 
@@ -22,9 +26,22 @@ public class PlayerMovement : NetworkBehaviour
         transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, rotationAngle, 0), .1f);
     }
 
+    private void Move(Vector2 backgroundPos, Vector2 handlePos)
+    {
+        transform.Translate(GetMovementDirection(backgroundPos, handlePos) * _movementSpeed * Runner.DeltaTime, Space.World);
+    }
+
     private float GetRotationAngle(Vector2 initialVector, Vector2 lastVector)
     {
-        Vector2 DifferenceVectorNormalized = (lastVector - initialVector);
-        return Mathf.Atan2(-DifferenceVectorNormalized.y, DifferenceVectorNormalized.x) * Mathf.Rad2Deg;
+        Vector2 DifferenceVector = (lastVector - initialVector);
+        return Mathf.Atan2(-DifferenceVector.y, DifferenceVector.x) * Mathf.Rad2Deg;
+    }
+
+    private Vector3 GetMovementDirection(Vector2 initialVector, Vector2 lastVector)
+    {
+        Vector2 DifferenceVectorNormalized = (lastVector - initialVector).normalized;
+        Vector3 MovementDirection = new Vector3(DifferenceVectorNormalized.x, 0 ,DifferenceVectorNormalized.y);
+        
+        return MovementDirection;
     }
 }
