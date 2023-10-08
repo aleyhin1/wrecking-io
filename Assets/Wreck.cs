6d29a1f8c0b166b3bf386a055ee6dd2808f1fa29
aleyhin1 <aleyhin1@gmail.com>
2023-10-08 21:53:07 +0300

    Pregame room reimplemented

--- Assets/Wreck.cs
+++ Assets/Wreck.cs
@@ -6,7 +6,10 @@ public class Wreck : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        collision.rigidbody.AddForceAtPosition(collision.relativeVelocity * 10, collision.GetContact(0).point, ForceMode.Force);
-        Debug.Log(collision.relativeVelocity);
+        if (collision.rigidbody != null)
+        {
+            collision.rigidbody.AddForceAtPosition(collision.relativeVelocity * 10, collision.GetContact(0).point, ForceMode.Impulse);
+            Debug.Log(collision.relativeVelocity);
+        }
     }
 }
